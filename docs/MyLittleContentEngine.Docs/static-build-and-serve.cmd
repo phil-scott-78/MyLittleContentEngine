a881e4f74ab720b1c87b1ad5899ccd69403805c8
Phil Scott <phil.scott@gmail.com>
2025-07-04 13:40:20 -0400

    trying to be more explicit about production

--- docs/MyLittleContentEngine.Docs/static-build-and-serve.cmd
+++ docs/MyLittleContentEngine.Docs/static-build-and-serve.cmd
@@ -1,3 +1,3 @@
 dotnet clean
-dotnet run --environment BaseUrl="/mybase/" -- build 
+dotnet run --environment ASPNETCORE_ENVIRONMENT=Production --environment BaseUrl="/mybase/" -- build 
 dotnet serve -d output --path-base mybase --default-extensions:.html
