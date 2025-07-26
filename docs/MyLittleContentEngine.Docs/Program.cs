12fbe4adaa9a928aca4e43877b8e339f2be731d3
Phil Scott <phil.scott@gmail.com>
2025-07-26 14:51:44 -0400

    adding inter to docs

--- docs/MyLittleContentEngine.Docs/Program.cs
+++ docs/MyLittleContentEngine.Docs/Program.cs
@@ -17,6 +17,7 @@ builder.Services.AddDocSite(_ => new DocSiteOptions(args)
     IncludeNamespaces = ["MyLittleContentEngine"],
     ExcludeNamespaces = ["MyLittleContentEngine.Tests"],
     DisplayFontFamily = "Lexend, sans-serif",
+    BodyFontFamily = "Inter, sans-serif",
     ExtraStyles = """
                   @font-face {
                     font-family: 'Lexend';
@@ -24,6 +25,15 @@ builder.Services.AddDocSite(_ => new DocSiteOptions(args)
                     font-weight: 100 900;
                     font-display: swap;
                     src: url(fonts/lexend.woff2) format('woff2');
+                    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+                  }
+                  
+                  @font-face {
+                    font-family: 'Inter';
+                    font-style: normal;
+                    font-weight: 100 900;
+                    font-display: swap;
+                    src: url(fonts/inter.woff2) format('woff2');
                     unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
                   }
                   """
